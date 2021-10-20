44c06516ae947fe40789c39c443521baa6124363
yqureishy <yqureishy1993@gmail.com>
2021-10-20 15:18:02 -0500

    Created packages - config, controller, filter, model, repository, service, util

--- MyYelp/src/main/java/com/cognixia/jump/model/User.java
+++ MyYelp/src/main/java/com/cognixia/jump/model/User.java
@@ -1,5 +1,105 @@
 package com.cognixia.jump.model;
 
-public class User {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	public enum Role {
+		ROLE_USER, ROLE_ADMIN
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(unique = true, nullable = false)
+	private String username;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(unique = true, nullable = false)
+	private String email;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Role role;
+
+	public User(Long id, String username, String password, String email, Role role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.role = role;
+	}
+	
+	public User() {
+		this(-1L, "N/A", "N/A", "N/A", Role.ROLE_USER);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", email=" + email + ", role="
+				+ role + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
