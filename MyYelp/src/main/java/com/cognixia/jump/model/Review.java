43198c5b77c19b7afe6a99771a59d3cd45998c6f
paskualino <paskualinomjeda@gmail.com>
2021-10-20 16:49:25 -0400

    Review model

--- MyYelp/src/main/java/com/cognixia/jump/model/Review.java
+++ MyYelp/src/main/java/com/cognixia/jump/model/Review.java
@@ -1,5 +1,109 @@
 package com.cognixia.jump.model;
 
-public class Review {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class Review implements Serializable{
+
+	public static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+		
+	@Column(nullable = false)
+	private String title;
+	
+	@Column(nullable = false)
+	private String body;
+		
+	@Column(nullable = false)
+	private Double rating;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "restaurant_id", referencedColumnName = "id")
+	private Restaurant restaurant;
+
+	public Review(Long id, String title, String body, Double rating, User user, Restaurant restaurant) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.body = body;
+		this.rating = rating;
+		this.user = user;
+		this.restaurant = restaurant;
+	}
+
+	
+	public Review() {
+		this(-1L, "N/A", "N/A", 0.0, null,null);
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public Double getRating() {
+		return rating;
+	}
+
+	public void setRating(Double rating) {
+		this.rating = rating;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Restaurant getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [id=" + id + ", title=" + title + ", body=" + body + ", rating=" + rating + ", user=" + user
+				+ ", restaurant=" + restaurant + "]";
+	}
+	
+	
 }
